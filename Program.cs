c33770965732f254a49206f248426a97ba822ee1
tpfields <tpfields@proton.me>
2025-08-12 19:11:25 -0400

    added ContosoPets project

--- Program.cs
+++ Program.cs
@@ -725,8 +725,192 @@ namespace Grades
         }
 
         public static void ContosoPets()
-        { 
-            
+        {
+            // the ourAnimals array will store the following: 
+            string animalSpecies = "",
+                animalID = "",
+                animalAge = "",
+                animalPhysicalDescription = "",
+                animalPersonalityDescription = "",
+                animalNickname = "";
+
+            // variables that support data entry
+            int maxPets = 8;
+            string? readResult;
+            int menuSelection = 0;
+
+            // array used to store runtime data, there is no persisted data
+            string[,] ourAnimals = new string[maxPets, 6];
+
+            // create some initial ourAnimals array entries
+            for (int i = 0; i < maxPets; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        animalSpecies = "dog";
+                        animalID = "d1";
+                        animalAge = "2";
+                        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+                        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                        animalNickname = "lola";
+                        break;
+                    case 1:
+                        animalSpecies = "dog";
+                        animalID = "d2";
+                        animalAge = "9";
+                        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+                        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+                        animalNickname = "loki";
+                        break;
+                    case 2:
+                        animalSpecies = "cat";
+                        animalID = "c3";
+                        animalAge = "1";
+                        animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+                        animalPersonalityDescription = "friendly";
+                        animalNickname = "Puss";
+                        break;
+                    case 3:
+                        animalSpecies = "cat";
+                        animalID = "c4";
+                        animalAge = "?";
+                        animalPhysicalDescription = "";
+                        animalPersonalityDescription = "";
+                        animalNickname = "";
+                        break;
+                    default:
+                        animalSpecies = "";
+                        animalID = "";
+                        animalAge = "";
+                        animalPhysicalDescription = "";
+                        animalPersonalityDescription = "";
+                        animalNickname = "";
+                        break;
+                }
+
+                ourAnimals[i, 0] = "ID #: " + animalID;
+                ourAnimals[i, 1] = "Species: " + animalSpecies;
+                ourAnimals[i, 2] = "Age: " + animalAge;
+                ourAnimals[i, 3] = "Nickname: " + animalNickname;
+                ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+                ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+            }
+
+            // display the top-level menu options
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+                Console.WriteLine(" 1. List all of our current pet information");
+                Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+                Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+                Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+                Console.WriteLine(" 5. Edit an animal’s age");
+                Console.WriteLine(" 6. Edit an animal’s personality description");
+                Console.WriteLine(" 7. Display all cats with a specified characteristic");
+                Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+                Console.WriteLine();
+                Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+                readResult = Console.ReadLine()?.ToLower().Trim();
+                if (readResult != null)
+                {
+                    int.TryParse(readResult, out menuSelection);
+                }
+
+                Console.WriteLine($"You selected menu option {readResult}.");
+                Console.WriteLine("Press the Enter key to continue");
+
+                Console.ReadLine();
+
+                switch (menuSelection)
+                {
+                    case 1:
+                        for (int i = 0; i < maxPets; i++)
+                        {
+                            string currentPetId = ourAnimals[i, 0];
+                            int idPosition = currentPetId.LastIndexOf(" ");
+                            string currentId = currentPetId.Substring(idPosition).Trim();
+                            if (currentId != "")
+                            {
+                                //Console.WriteLine($"Existing pet data found for {ourAnimals[i, 0]}");
+                                for (int j = 0; j < 5; j++)
+                                {
+                                    Console.WriteLine(ourAnimals[i, j]);
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        break;
+                    case 2:
+                        string? anotherPet = "y";
+                        int petCount = 0;
+
+                        while (anotherPet == "y")
+                        {
+                            int petsWithData = 0;
+
+                            for (int i = 0; i < maxPets; i++)
+                            {
+                                if (ourAnimals[i, 0] != "ID #: ")
+                                {
+                                    petsWithData++;
+                                }
+                            }
+
+                            Console.WriteLine($"{petsWithData} pet records have data.");
+
+                            if (petsWithData < maxPets)
+                            {
+                                Console.WriteLine($"There are {petsWithData} pets with data, this is less than the max pet count of {maxPets}!");
+
+                                Console.WriteLine("Enter Animal ID:");
+                                ourAnimals[petsWithData, 0] = "ID #: " + Console.ReadLine();
+                                Console.WriteLine("Enter Animal Species:");
+                                ourAnimals[petsWithData, 1] = "Species: " + Console.ReadLine();
+                                Console.WriteLine("Enter Animal Age:");
+                                ourAnimals[petsWithData, 2] = "Age: " + Console.ReadLine();
+                                Console.WriteLine("Enter Animal Nickname:");
+                                ourAnimals[petsWithData, 3] = "Nickname: " + Console.ReadLine();
+                                Console.WriteLine("Enter Animal Physical Description:");
+                                ourAnimals[petsWithData, 4] = "Physical description: " + Console.ReadLine();
+
+                                petCount++;
+
+                                Console.WriteLine($"Pet Data Entered {petCount} have been entered so far, press any key to continue");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Cannot add new pets, please remove some pets first!");
+                                break;
+                            }
+                            Console.WriteLine("Would you like to enter another pet? Enter 'y' or 'n'");
+                            anotherPet = Console.ReadLine();
+                            anotherPet = anotherPet?.ToLower().Trim();
+                        }
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("Press the Enter key to continue");
+                Console.ReadLine();
+            }
+            while (readResult != "exit");
         }
     }
 }
